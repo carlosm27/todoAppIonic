32887c8ed9eb544ca41bb628de68b51c473a878c
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-03-18 11:15:35 -0400

    Create tasks.tsx

---
+++ src/pages/tasks.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+import ListTask from '../components/taskList';
+
+let dummyData = [{"id":"1", "body":"Get milk" }, {"id":"2", "body":"Wash car" }, {"id":"3", "body":"Start coding"}]
+
+
