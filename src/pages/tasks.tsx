313eab7fd5ae2158e5e52e07161a00ea240a2878
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-03-18 19:32:14 -0400

    Update tasks.tsx

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
-import ListTask from '../components/taskList';
+import TaskList from '../components/taskList';
 
-let dummyData = [{"id":"1", "body":"Get milk" }, {"id":"2", "body":"Wash car" }, {"id":"3", "body":"Start coding"}]
+let dummyData = [{"id":"1", "body":"Get milk" }, {"id":"2", "body":"Wash car" }, {"id":"3", "body":"Start coding"}];
 
 
+export const AddTask = () => {
+    let [tasks, setTasks] = useState(dummyData)
+    return (
+        <div>
+            <Link to = {'/add'}>Add</Link>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}><TaskList/></li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default AddTask;
