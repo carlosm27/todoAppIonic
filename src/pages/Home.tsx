e1a026925a71a43f27ced3045ad50a91202e9600
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-03-18 19:32:01 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import TaskList from '../components/taskList'
+import TaskList from '../components/taskList';
+import AddTask from '../components/AddList';
+
+
+
 const Home: React.FC = () => {
   return (
     <IonPage>
@@ -16,7 +20,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <TaskList/>
+        <AddTask/>
         <ExploreContainer />
       </IonContent>
     </IonPage>
