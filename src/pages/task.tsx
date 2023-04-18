16211a19f41d150bdccdffc86badc694e6ee6771
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-18 12:20:18 -0400

    Create task.tsx

---
+++ src/pages/task.tsx
@@ -0,0 +1,5 @@
+import React, {useState, useEffect} from 'react'
+import { useParams, Link } from 'react-router-dom'
+
+let dummyData = [{"id":"1", "body":"Get milk" }, {"id":"2", "body":"Wash car" }, {"id":"3", "body":"Start coding"}]
+
